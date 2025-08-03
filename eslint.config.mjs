a203b0deffe64fb7e4238455557af838d13dd7f2
Michal Saraz <saraz.m@seznam.cz>
2025-08-03 13:43:43 +0200

    chore: update configuration files to ignore SQL files and enhance TypeScript settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'any',
-        normal: 'always',
-        component: 'always'
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
       },
-      svg: 'always',
-      math: 'always'
-    }],
+    ],
   },
-})
+  ignores: [
+    '**/*.sql', // Ignoruje všechny SQL soubory (včetně database/ složky)
+  ],
+});
